dd1322ebb2891dab36087aafebe2f346efeba1c3
Md Sorwar Hosen <sorwar@newroztech.com>
2023-08-20 19:00:45 +0600

    timer service classed added

--- merchantSDK/src/main/java/com/fastpay/payment/service/utill/ShareData.java
+++ merchantSDK/src/main/java/com/fastpay/payment/service/utill/ShareData.java
@@ -1,5 +1,7 @@
 package com.fastpay.payment.service.utill;
 
+import com.fastpay.payment.BuildConfig;
+
 public class ShareData {
 
     // Constant text
@@ -14,5 +16,14 @@ public class ShareData {
     public static String KURDISH_LANG = "ku";
 
     public static final int PERMISSION_REQUEST_CODE = 100;
+
+    public static final String KEY_FINISHING_TIME = "key_finishing_time";
+    public static final int PAYMENT_TERMS_TIME_OUT = 188;
+
+    public static long SESSION_TIME_OUT_VALUE = BuildConfig.DEBUG ? (1000 * 10) : (1000 * 60 * 4);
+
+    public static long USER_SESSION_TIMER_INTERVAL = 1L; // In second
+    public static long USER_SESSION_TIMER_TARGET = BuildConfig.DEBUG ? 10  : (4 * 60); // In second
+    public static String INTENT_USER_SESSION_FINISHED = "user_session_finished";
 
 }
---
+++ merchantSDK/src/main/java/com/fastpay/payment/service/utill/StoreInformationUtil.java
@@ -0,0 +1,33 @@
+package com.fastpay.payment.service.utill;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class StoreInformationUtil {
+
+    private static String PREF_NAME = "fast_pay_sdk_pref";
+
+    private static SharedPreferences getPref(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences;
+    }
+
+    public static boolean saveLongData(Context context, String key, Long data) {
+        SharedPreferences.Editor editor = getPref(context).edit();
+        editor.putLong(key, data);
+        return editor.commit();
+    }
+
+
+    public static Long getLongData(Context context, String key,Long defaultValue) {
+        SharedPreferences sharedPreferences = getPref(context);
+        return sharedPreferences.getLong(key, defaultValue);
+    }
+
+    public static void clearKey(Context context,String key){
+        SharedPreferences.Editor editor = getPref(context).edit();
+        editor.remove(key);
+        editor.apply();
+    }
+}
