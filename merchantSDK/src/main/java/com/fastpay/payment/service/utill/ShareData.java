5c1466ed87cbac90fcf4ef56490e53b60d710bc8
Md Sorwar Hosen <sorwar@newroztech.com>
2023-08-20 15:12:57 +0600

    New Share data added

--- merchantSDK/src/main/java/com/fastpay/payment/service/utill/ShareData.java
+++ merchantSDK/src/main/java/com/fastpay/payment/service/utill/ShareData.java
@@ -6,6 +6,8 @@ public class ShareData {
     public static final String DATE = "Date:";
     public static final String CURRENCY_IQD = "IQD";
 
+    public static final String KEY_OTP_MESSAGE = "keyOtpMessage";
+
     public static String COUNTRY_CODE = "+964";
     public static String ENGLISH_LANG = "en";
     public static String ARABIC_LANG = "ar";
